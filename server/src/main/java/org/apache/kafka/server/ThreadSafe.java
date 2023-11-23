df789721be0ab357fe6873afc1557af46def6cf5
SimonJorgensenMancofi <simon.jorgensen@mancofi.dk>
2023-11-23 10:00:50 +0100

    Work

--- server/src/main/java/org/apache/kafka/server/ThreadSafe.java
+++ server/src/main/java/org/apache/kafka/server/ThreadSafe.java
@@ -1,6 +1,6 @@
 package org.apache.kafka.server;
 
-public import java.lang.annotation.ElementType;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
